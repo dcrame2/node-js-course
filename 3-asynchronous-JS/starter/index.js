bb1dd08916a11cca5a0063d4b47a28bc03dbcfd0
Dylan Cramer <dcrame2@gmail.com>
2023-05-19 13:13:10 -0500

    async await finished

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -23,19 +23,51 @@ const getDogPic = async () => {
   try {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
 
-    await writeFilePro("dog-img.txt", res.body.message);
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((img) => {
+      return img.body.message;
+    });
+    console.log(imgs);
+
+    await writeFilePro("dog-img.txt", imgs.join("\n"));
     console.log("Random dog image saved");
   } catch (err) {
     console.log(err.message);
+    throw err;
   }
+  return `2: READY MFFF`;
 };
-getDogPic();
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics!");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+
+// console.log("1: Will get dog pics!");
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log("3: Done getting dog pics!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
