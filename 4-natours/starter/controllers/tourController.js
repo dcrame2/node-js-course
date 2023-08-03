172482e1ec5bdeb00be8ed2c017c5b4d599d3d26
Dylan Cramer <dcramer@impactnetworking.com>
2023-08-03 14:43:13 -0500

    query

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,21 +2,27 @@ const Tour = require('../models/tourModels');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    const query = await Tour.find(queryObj);
 
-    const tours = await Tour.find(queryObj);
+    console.log(req.query);
 
-    // const tours = await Tour.find()
+    //{ difficulty: 'easy', duration: { $gte: 5}}
+
+    // const query = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
-    // const tours = await Tour.find();
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
