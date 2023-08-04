8b1a84c6a491595a818d409602ccefaa3580d3d8
Dylan Cramer <dcramer@impactnetworking.com>
2023-08-04 13:49:42 -0500

    querying filters through api

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -14,17 +14,37 @@ exports.getAllTours = async (req, res) => {
       /\b(gte|gt|lte|lt)\b/g,
       (match) => `$${match}`
     );
-    console.log(req.query);
 
     let query = Tour.find(JSON.parse(queryString));
 
     // 2) Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
-      // sort('price ratingAverage')
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query.select('-__v');
+    }
+
+    // 4) Pagination
+
+    // page=2&limit=10, 1-10 = page 1, 11-20 = page 2, etc.
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist.');
     }
 
     // EXECUTE QUERY
